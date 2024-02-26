dfaee768b0e6fb5052f03d2bf66a5aaa3a766abe
GustavoRKraemer <130578547+GustavoRKraemer@users.noreply.github.com>
2024-02-26 10:51:23 -0300

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,6 @@
+let num = 7
+let numSecreto = 10
+let resultado = num == numSecreto
+console.log(`o resultado é ${resultado}`)
+
+
---
+++ objetos.js
@@ -0,0 +1,15 @@
+let nome ={
+    nome:`gustavo`,
+    documento:`RG`,
+    estadoCivil:`solteiro`,
+    idade:`17`,
+    endereço:`Novo Horizonte`
+}
+console.log(nome);
+
+console.log(nome.nome);
+console.log(nome.documento);
+console.log(nome.estadoCivil);
+console.log(nome.idade);
+console.log(nome.endereço);
+
